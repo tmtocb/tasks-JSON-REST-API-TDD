b1e351cd32f4bdb222cc1c1adaafd92a8b3d5c65
tmtocb <kittz.esport@gmail.com>
2021-02-02 02:45:49 +0100

    add Points controller CRUD actions and routing with TDD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    resources :points
+  end
 end
--- spec/requests/points_request_spec.rb
+++ spec/requests/points_request_spec.rb
@@ -1,5 +1,121 @@
 require 'rails_helper'
 
 RSpec.describe "Points API", type: :request do
+  let!(:task) { create(:task) }
+  let!(:points) { create_list(:point, 20, task_id: task.id) }
+  let(:task_id) { task.id }
+  let(:id) { points.first.id }
+                     
+  describe 'GET index' do
+    before { get "/tasks/#{task_id}/points" }
 
+    context 'when task exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns all task points' do
+        expect(json.size).to eq(20)
+      end
+    end
+
+    context 'when task does not exist' do
+      let(:task_id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Task/)
+      end
+    end
+  end
+
+  describe 'GET show' do
+    before { get "/tasks/#{task_id}/points/#{id}" }
+
+    context 'when task point exists' do
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the item' do
+        expect(json['id']).to eq(id)
+      end
+    end
+
+    context 'when task point does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Point/)
+      end
+    end
+  end
+
+  describe 'POST create' do
+    let(:valid_attributes) { { name: 'Rails', done: false } }
+
+    context 'when request attributes are valid' do
+      before { post "/tasks/#{task_id}/points", params: valid_attributes }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when an invalid request' do
+      before { post "/tasks/#{task_id}/points", params: {} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns a failure message' do
+        expect(response.body).to match(/Validation failed: Name can't be blank/)
+      end
+    end
+  end
+
+  describe 'PUT update' do
+    let(:valid_attributes) { { name: 'Laravel' } }
+
+    before { put "/tasks/#{task_id}/points/#{id}", params: valid_attributes }
+
+    context 'when point exists' do
+      it 'returns status code 204' do
+        expect(response).to have_http_status(204)
+      end
+
+      it 'updates the point' do
+        updated_point = Point.find(id)
+        expect(updated_point.name).to match(/Laravel/)
+      end
+    end
+
+    context 'when the item does not exist' do
+      let(:id) { 0 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Point/)
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    before { delete "/tasks/#{task_id}/points/#{id}" }
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
