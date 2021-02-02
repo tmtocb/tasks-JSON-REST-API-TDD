3d244a8eb73856432a2c1f844e365f257477736d
tmtocb <kittz.esport@gmail.com>
2021-02-02 03:17:55 +0100

    add secure password to Users, add User association with TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password_digest) }
   end
+
+  describe '#associations' do
+    it { should have_many(:tasks) }
+  end
 end
