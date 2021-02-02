b903d202481a132bd7101757ef7a42a2ce5ab8b7
tmtocb <kittz.esport@gmail.com>
2021-02-02 01:29:08 +0100

    add Point model validations with TDD

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,3 +1,4 @@
 class Point < ApplicationRecord
   belongs_to :task
+  validates_presence_of :name
 end
--- spec/models/point_spec.rb
+++ spec/models/point_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Point, type: :model do
+
   describe '#columns' do
     it { is_expected.to have_db_column(:name) }
     it { is_expected.to have_db_column(:done) }
     it { is_expected.to have_db_column(:task_id) }
+  end
+
+  describe '#validations' do
+    it { should validate_presence_of(:name) }
   end
 end
