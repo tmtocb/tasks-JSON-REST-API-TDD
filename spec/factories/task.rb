e8ae34d197908db63ea00c4c2d6ff04d94fe6584
tmtocb <kittz.esport@gmail.com>
2021-02-02 02:13:59 +0100

    add Tasks controller CRUD actions and routing with TDD

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
     title { Faker::Lorem.word } 
-    created_by { Faker::Number.number(10) }
+    created_by { Faker::Number.number(digits: 10) }
   end
 end
