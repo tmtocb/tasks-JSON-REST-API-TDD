020c418a6ee999363453115e9fe32a77b1b0d825
tmtocb <kittz.esport@gmail.com>
2021-02-02 03:14:04 +0100

    add User model validations with TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe User, type: :model do
     it { is_expected.to have_db_column(:email) }
     it { is_expected.to have_db_column(:password_digest) }
   end
+
+  describe '#validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+  end
 end
